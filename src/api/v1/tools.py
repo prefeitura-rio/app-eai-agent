99f81c5f10e1756961391840120feae27573a80e
Gabryelle Soares <gabryelle.ss3@gmail.com>
2025-05-15 16:22:53 -0300

    add: typesense search tool

--- src/api/v1/tools.py
+++ src/api/v1/tools.py
@@ -1,16 +1,25 @@
-from fastapi import APIRouter, Depends, Body
-from typing import Optional
+import requests
+from fastapi import APIRouter, Depends, Query, HTTPException
+from loguru import logger
+
 
 from src.core.security.dependencies import validar_token
 from src.schemas.letta_schema import MessageRequest, MessageResponse
 from src.services.letta.agents.tasks.google_search import google_search
 
-router = APIRouter(prefix="/letta/tools", tags=["Letta", "Tools"], dependencies=[Depends(validar_token)])
+router = APIRouter(
+    prefix="/letta/tools",
+    tags=["Letta", "Tools"],
+    dependencies=[Depends(validar_token)],
+)
+
 
 @router.get("/google_search", name="Busca Google")
 async def google_search(
     query: str = Query(..., description="Texto da consulta"),
-    model: str = Query("gemini-2.5-pro-preview-03-25", description="Modelo Gemini a ser usado"),
+    model: str = Query(
+        "gemini-2.5-pro-preview-03-25", description="Modelo Gemini a ser usado"
+    ),
 ):
     try:
         gemini_service = GeminiService()
@@ -18,14 +27,16 @@ async def google_search(
 
         system_prompt = ""
         prompt = {"role": "user", "parts": [{"text": actual_query}]}
-        
+
         logger.info("Enviando consulta para o Gemini com system_instruction")
         response, token_usage = await generate_content_async(
             prompt, model=model, system_instruction=system_prompt
         )
 
         if not response or not response.candidates:
-            raise HTTPException(status_code=500, detail="Falha ao gerar resposta do Gemini")
+            raise HTTPException(
+                status_code=500, detail="Falha ao gerar resposta do Gemini"
+            )
 
         result = response.candidates[0].content.parts[0].text.strip()
 
@@ -36,4 +47,32 @@ async def google_search(
         }
     except Exception as e:
         logger.error(f"Erro ao gerar resposta do Gemini: {e}")
-        raise HTTPException(status_code=500, detail=f"Erro ao processar a requisição: {str(e)}")
+        raise HTTPException(
+            status_code=500, detail=f"Erro ao processar a requisição: {str(e)}"
+        )
+
+
+@router.get("/typesense_search", name="Busca Typesense")
+async def typesense_search(query: str = Query(..., description="Texto da consulta")):
+    try:
+        endpoint = f"https://busca.dados.rio/search/multi"
+        headers = {"x-recaptcha-token": "teste"}
+        params = {"q": query, "cs": "1746,carioca-digital"}
+
+        response = requests.get(endpoint, headers=headers, params=params)
+        return response.json()
+    except Exception as e:
+        logger.error(f"Erro: {e}")
+        raise HTTPException(
+            status_code=500, detail=f"Erro ao processar a requisição: {str(e)}"
+        )
+
+@router.post("/summarize", name="Sumarizar resposta final")
+async def summarize_response(query: str = Query(..., description="Texto da consulta")):
+    try:
+        pass
+    except Exception as e:
+        logger.error(f"Erro: {e}")
+        raise HTTPException(
+            status_code=500, detail=f"Erro ao processar a requisição: {str(e)}"
+        )
