d917dede757d40300f4910f9641e4513bf628ac0
Bruno Almeida <brunodepauloalmeida@gmail.com>
2025-05-15 14:19:48 -0300

    add: tools base files

--- src/api/v1/__init__.py
+++ src/api/v1/__init__.py
@@ -1,7 +1,10 @@
 from fastapi import APIRouter
 from src.api.v1.test_letta import router as letta_router
 from src.api.v1.webhook import router as webhook_router
+from src.api.v1.tools import router as tools_router
+
 
 router = APIRouter(prefix="/v1")
 router.include_router(letta_router)
 router.include_router(webhook_router)
+router.include_router(tools_router)
