1d51e2e436a4fa50f2f93843daead8902d44dcba
Gabryelle Soares <gabryelle.ss3@gmail.com>
2025-06-04 16:19:33 -0300

    refactor: upload dataset to phoenix

--- src/evaluations/letta/phoenix/training_dataset/upload_dataset.py
+++ src/evaluations/letta/phoenix/training_dataset/upload_dataset.py
@@ -1,18 +1,17 @@
 import os
 import sys
-import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../../../')))
-
-from src.config import env
 from datetime import datetime
+
 import pandas as pd
 import phoenix as px
 
-api_key = env.GEMINI_API_KEY
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../../../')))
 
-phoenix_client = px.Client(endpoint="http://34.60.92.205:6006")
+from src.config import env
 
-training_dataset = pd.read_csv("src/evaluations/letta/phoenix/training_dataset/servicos_perguntas_respostas.csv", encoding="utf-8")
+phoenix_client = px.Client(endpoint=env.PHOENIX_ENDPOINT)
+DATASET_PATH = "src/evaluations/letta/phoenix/training_dataset/servico_perguntas_respostas.csv"
+training_dataset = pd.read_csv(DATASET_PATH, encoding="utf-8")
 
 keeping_columns = [
     "pergunta_individual",
@@ -25,20 +24,33 @@ keeping_columns = [
     "orgao_gestor",
 ]
 
-training_dataset = training_dataset[keeping_columns]
-training_dataset = training_dataset.dropna(subset=["pergunta_individual", "resposta_ideal_individual"])
-
-# Rename columns pergunta_individual and resposta_ideal_invidividual
-training_dataset = training_dataset.rename(
-    columns={
-        "pergunta_individual": "pergunta",
-        "resposta_ideal_individual": "resposta_ideal",
-    }
+training_dataset = (
+    training_dataset[keeping_columns]
+    .dropna(subset=["pergunta_individual", "resposta_ideal_individual"])
+    .rename(
+        columns={
+            "pergunta_individual": "pergunta",
+            "resposta_ideal_individual": "resposta_ideal",
+        }
+    )
 )
 
 now = datetime.now().strftime("%Y-%m-%d")
-# create a dataset consisting of input questions and expected outputs
-dataset = phoenix_client.upload_dataset(dataframe=training_dataset.sample(30), 
-                                   dataset_name=f"Typesense_IA_Dataset-{now}", 
-                                   input_keys=["pergunta", "titulo", "descricao", "informacoes_complementares", "prazo_esperado", "resumo_whatsapp", "orgao_gestor"], 
-                                   output_keys=["resposta_ideal"])
+dataset_name = f"Typesense_IA_Dataset-{now}"
+
+dataset = phoenix_client.upload_dataset(
+    dataframe=training_dataset.sample(30), 
+    dataset_name=dataset_name, 
+    input_keys=[
+        "pergunta",
+        "titulo",
+        "descricao",
+        "informacoes_complementares",
+        "prazo_esperado",
+        "resumo_whatsapp",
+        "orgao_gestor",
+    ], 
+    output_keys=["resposta_ideal"],
+)
+
+print(f"Dataset '{dataset_name}' enviado com sucesso para o Phoenix!")
