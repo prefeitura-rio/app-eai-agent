c4749f767caa89903e266cfc3087bd9d1fab13a3
Bruno Almeida <brunodepauloalmeida@gmail.com>
2025-06-12 11:10:47 -0300

    feat: update dataset paths and adjust columns for GPT integration

--- src/evaluations/letta/phoenix/training_dataset/create_dataset.py
+++ src/evaluations/letta/phoenix/training_dataset/create_dataset.py
@@ -6,26 +6,126 @@ import typesense
 import pandas as pd
 from tqdm import tqdm
 import google.generativeai as genai
+import sys
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../../../../")))
 
 from src.config import env
+import pprint
 
 tqdm.pandas()
 
-TYPESENSE_API_KEY = env.TYPESENSE_API_KEY
 GEMINI_API_KEY = env.GEMINI_API_KEY
+
+TOP_SERVICOS_CARIOCA_DIGITAL = [
+    'IPTU – Parcelamento 2024 – Solicitação e emissão de boletos',
+    'IPTU – Autenticidade da Certidão de Situação Fiscal e Enfitêutica do Imóvel',
+    'IPTU 2024 e anteriores – cobrança na Fazenda',
+    'IPTU – Autenticidade da Certidão de Elementos Cadastrais',
+    'IPTU – Notificação de Lançamento (inclusive guias extraordinárias)',
+    'IPTU – Regularização de Recolhimentos (parcelamento, restituição, alegação e transposição)',
+    'IPTU – Alteração de nome do proprietário',
+    'IPTU – Certidão de Pagamentos e débito automático',
+    'CADRio Agendamento',
+    'Censo Cadastral Previdenciário – Previ-Rio',
+    'ITBI – Simulação de Valor e Pedido de Guia',
+    'ITBI – Impressão de guia para pagamento',
+    'ITBI – Emissão da certidão de pagamento',
+    'Emissão de certidão de dívida ativa',
+    'Multa de Trânsito – Consulta de Multa',
+    'SMPDA – Programa Bicho Rio – Esterilização gratuita de cães e gatos',
+    'Infração ao Código Disciplinar de Transporte de Passageiros – Defesa Prévia',
+]
+
+TOP_SERVICOS_1746 = [
+    "Fiscalização de má conduta do motorista/despachante",
+    "Fiscalização de má condição do ônibus",
+    "Reparo de Luminária",
+    "Solicitação de balizamento de trânsito",
+    "Verificação de ar condicionado inoperante no ônibus",
+    "Solicitação de correção de falhas e de cadastro no portal e app 1746",
+    "Fiscalização de obras em imóvel privado",
+    "Solicitação de transporte da gestante para a maternidade",
+    "Remoção de árvore em logradouro",
+    "Avaliação de risco de queda da árvore",
+    "Varrição de logradouro",
+    "Remoção de resíduos de poda",
+    "Notificação ao proprietário para limpeza de resíduos em terreno baldio",
+    "Limpeza de ralos",
+    "Limpeza de praças e parques",
+    "Controle de roedores e caramujos africanos",
+    "Verificação de frequência irregular da coleta domiciliar com retirada do resíduo",
+    "Solicitação de orientações sobre o alvará pela internet",
+    "Vistoria em imóvel com rachadura e infiltração",
+    "Vistoria em ameaça de desabamento de estrutura",
+    "Fiscalização de obstáculo fixo na calçada",
+    "Manutenção de mobiliário, brinquedos e equipamentos esportivos em praças e parques públicos",
+    "Fiscalização de buraco na calçada",
+    "Reposição de tampão ou grelha",
+    "Vistoria técnica em situações de maus tratos de animais domésticos",
+    "Reparo de buraco, deformação ou afundamento na pista",
+    "Fiscalização de perturbação do sossego",
+    "Fiscalização de atividades econômicas sem alvará",
+    "Remoção de veículo abandonado em via pública",
+    "Manutenção/Desobstrução de ramais de águas pluviais e ralos",
+    "Vistoria em foco de Aedes Aegypti (Dengue, Chikungunya e Zika)",
+    "Capina em logradouro",
+    "Reparo de sinal de trânsito apagado",
+    "Remoção de resíduos no logradouro",
+    "Poda de árvore em logradouro",
+    "Remoção de entulho e bens inservíveis",
+    "Resgate de animais silvestres",
+    "Fiscalização de estacionamento irregular de veículo",
+    "Fiscalização/Inspeção de feiras livres, feiras de artes, feiras especiais e feiras de ambulantes",
+    "Informações sobre os Centros de Referência Especializado para população em situação de rua",
+    "Pessoas em situação de rua",
+    "Fiscalização de obstáculo fixo na calçada",
+    "Fiscalização de buraco na calçada",
+    "Reparo de tampão ou poste danificado ou fora de prumo",
+    "Reparo de bloco de lâmpadas apagadas (Circuito Fora) na TransOeste",
+    "Reparo de bloco de lâmpadas acesas durante o dia (Circuito Direto) na TransOeste",
+    "Informações sobre fiscalização de irregularidades em linhas de ônibus",
+    "Verificação de alteração de itinerário de ônibus",
+    "Castração de cães e gatos agendamento on-line", # Pedido no carioca digital, achado aqui
+    "Cartão de estacionamento para idosos", # Pedido no carioca digital, achado aqui
+]
+
+def dataset_get_response_from_gpt(input:str) -> dict:
+    headers = {
+        "Content-Type": "application/json",
+        "api-key": env.OPENAI_API_KEY
+    }
+    
+    payload = {
+        "messages": [
+            {
+                "role": "user", 
+                "content": input,
+            }
+        ],
+        "temperature": 0.8,
+        "max_tokens": 256
+    }
+
+    url = f"{env.OPENAI_URL}openai/deployments/gpt-4.1/chat/completions?api-version=2024-02-15-preview"
+
+    response = requests.post(url, headers=headers, json=payload)
+    response.raise_for_status()
+    data = response.json()
+    return data["choices"][0]["message"]["content"]
 
 client = typesense.Client({
     "nodes": [{
-        "host": "staging.typesense.dados.rio",
+        "host": "services.staging.app.dados.rio",
         "port": "443", # "8108",
         "protocol": "https"
     }],
-    "api_key": TYPESENSE_API_KEY,
+    "api_key": env.TYPESENSE_CLIENT_API_KEY,
     "connection_timeout_seconds": 15
 })
 
 try:
-    r = requests.get("https://staging.typesense.dados.rio:443/health", timeout=10)
+    r = requests.get("https://services.staging.app.dados.rio:443/health", timeout=10)
     print(r.text)
 except Exception as e:
     print(e)
@@ -40,7 +140,7 @@ safety_settings = [
 ]
 
 generation_config = {
-    "temperature": 0.7,
+    "temperature": 0.6,
     "top_p": 0.95,
     "top_k": 64,
     "max_output_tokens": 8192,
@@ -48,7 +148,7 @@ generation_config = {
 }
 
 model = genai.GenerativeModel(
-    model_name="gemini-2.0-flash",
+    model_name="gemini-2.5-flash-preview-05-20",
     safety_settings=safety_settings,
     generation_config=generation_config
 )
@@ -72,6 +172,7 @@ def export_collections_to_csv(collection_name):
             break
 
         for hit in hits:
+            # print(f"O nome do documento é: {hit['document']['titulo']}")
             all_docs.append(hit['document'])
 
         query_params["page"] += 1
@@ -91,18 +192,24 @@ def export_collections_to_csv(collection_name):
 
 
 def generate_questions(row):
-    prompt = f"""Gere 5 perguntas que um cidadão poderia fazer sobre o serviço da prefeitura descrito abaixo. Retorne apenas as perguntas em uma lista sem numeração e uma por linha.
-    
-Título: {row['titulo']}
-Descrição: {row.get('descricao_texto_normalizado', '')}
-Informações complementares: {row.get('informacoes_complementares', '')}
-Prazo: {row.get('prazo_esperado', '')}
-Custo: {row.get('custo_do_servico', '')}
-"""
+    prompt = f"""Você é um desenvolvedor construindo cenários de testes para avaliar um mecanismo de busca de tópicos.
+    A partir do tópico abaixo crie 5 exemplos de perguntas relacionados ao tópico: {row['titulo']}. 
+    O usuário é uma pessoa comum,  usar tom informal.
+
+    O formato da resposta deve ser apenas uma lista de strings, onde cada string é uma pergunta.
+    Exemplo: 
+    ['pergunta_1', 'pergunta_2', 'pergunta_3', 'pergunta_4', 'pergunta_5']
+    """
+# Descrição: {row.get('descricao_texto_normalizado', '')}
+# Informações complementares: {row.get('informacoes_complementares', '')}
+# Prazo: {row.get('prazo_esperado', '')}
+# Custo: {row.get('custo_do_servico', '')}
+
 
     try:
-        response = model.generate_content(prompt)
-        return response.text.strip().split("\n")[:5]
+        # response = model.generate_content(prompt)
+        # return response.text.strip().split("\n")[:5]
+        return dataset_get_response_from_gpt(prompt)
     except:
         print(f"Erro ao gerar perguntas para '{row.get('titulo')}': {e}")
         return []
@@ -122,8 +229,7 @@ Não invente informações, URLs ou detalhes que não estejam explicitamente no 
 A informação que você quer pode não estar explícita, mas deve ser inferida a partir dos dados do serviço.
 Evite frases como "Com base nas informações fornecidas". Apenas use as informações.
 Caso você indique que o usuário consulte um site, procure enviar a URL do site, se disponível no contexto fornecido.
-Seja direto e responda à pergunta. Use marcadores (bullet points) se ajudar na clareza da resposta.
-
+Seja direto e responda à pergunta. Sua resposta será enviada para o Whatsapp do cidadão, então responda de forma que ele possa entender facilmente nesse canal.
 Contexto do Serviço:
 {contexto}
 
@@ -167,9 +273,10 @@ def process_row_for_responses(row):
         if isinstance(pergunta, str) and pergunta.strip():
             resposta = generate_ideal_response(row, pergunta)
             ideal_responses.append(resposta)
+            print(f"Pergunta: {pergunta}\nResposta Ideal: {resposta}\n")
         else:
             ideal_responses.append("Pergunta inválida ou vazia, resposta não gerada.")
-    
+
     return ideal_responses
 
 
@@ -177,7 +284,7 @@ def explode_dataframe(df, collection_name):
     expanded_data_list = []
 
     columns_to_replicate = [
-        col for col in df.columns if col not in ['perguntas_geradas', 'lista_respostas_ideais']
+        col for col in df.columns if col not in ['perguntas_geradas']#, 'lista_respostas_ideais']
     ]
 
     for index, row in tqdm(df.iterrows(), total=df.shape[0], desc=f"Explodindo {collection_name}"):
@@ -192,19 +299,20 @@ def explode_dataframe(df, collection_name):
             perguntas_list = []
 
         
-        try:
-            respostas_str = row['lista_respostas_ideais']
-            respostas_list = ast.literal_eval(respostas_str) if pd.notna(respostas_str) else []
-            if not isinstance(respostas_list, list):
-                print(f"Aviso: 'lista_respostas_ideais' na linha {index} não é uma lista. Conteúdo: {respostas_list}")
-                respostas_list = []
-        except (ValueError, SyntaxError):
-            print(f"Aviso: Falha ao parsear 'lista_respostas_ideais' na linha {index}.")
-            respostas_list = []
+        # try:
+        #     # respostas_str = row['lista_respostas_ideais']
+        #     # respostas_list = ast.literal_eval(respostas_str) if pd.notna(respostas_str) else []
+        #     respostas_list = row.get('lista_respostas_ideais', [])
+        #     if not isinstance(respostas_list, list):
+        #         print(f"Aviso: 'lista_respostas_ideais' na linha {index} não é uma lista. Conteúdo: {respostas_list}")
+        #         respostas_list = []
+        # except (ValueError, SyntaxError):
+        #     print(f"Aviso: Falha ao parsear 'lista_respostas_ideais' na linha {index}.")
+        #     respostas_list = []
 
-        num_pares = min(len(perguntas_list), len(respostas_list))
-        if len(perguntas_list) != len(respostas_list):
-            print(f"Aviso: Número de perguntas ({len(perguntas_list)}) e respostas ({len(respostas_list)}) difere para o serviço '{row.get('titulo', 'N/A')}' (linha {index}). Usando {num_pares} pares.")
+        num_pares = len(perguntas_list) # min(len(perguntas_list), len(respostas_list))
+        # if len(perguntas_list) != len(respostas_list):
+        #     print(f"Aviso: Número de perguntas ({len(perguntas_list)}) e respostas ({len(respostas_list)}) difere para o serviço '{row.get('titulo', 'N/A')}' (linha {index}). Usando {num_pares} pares.")
 
         for i in range(num_pares):
             new_expanded_row = {}
@@ -212,7 +320,7 @@ def explode_dataframe(df, collection_name):
                 new_expanded_row[col_name] = row[col_name]
             
             new_expanded_row['pergunta_individual'] = perguntas_list[i]
-            new_expanded_row['resposta_ideal_individual'] = respostas_list[i]
+            # ['resposta_ideal_individual'] = respostas_list[i]
             
             expanded_data_list.append(new_expanded_row)
         
@@ -224,8 +332,8 @@ def explode_dataframe(df, collection_name):
 
 
 def process_collection(collection_name):
-    input_csv = f"perguntas_baseadas_no_{collection_name}.csv"
-    output_csv = f"servicos_perguntas_respostas_{collection_name}.csv"
+    input_csv = f"perguntas_baseadas_no_{collection_name}_GPT.csv"
+    output_csv = f"servicos_perguntas_respostas_{collection_name}_GPT.csv"
 
     print(f"\nProcessando arquivo: {input_csv}...")
 
@@ -244,7 +352,14 @@ def process_collection(collection_name):
         return
 
     print(f"Gerando respostas ideais para {collection_name}...")
-    df['lista_respostas_ideais'] = df.progress_apply(process_row_for_responses, axis=1)
+    # df = df.head(1)
+    # df['lista_respostas_ideais'] = df.progress_apply(process_row_for_responses, axis=1)
+
+    # print(df.head(10))
+    
+    # Print the full value of the first 'lista_respostas_ideais' entry, no truncation
+    # pp = pprint.PrettyPrinter(width=200, compact=False)
+    # pp.pprint(df["lista_respostas_ideais"].iloc[0])
 
     df_exploded = explode_dataframe(df, collection_name)
 
@@ -260,7 +375,7 @@ def process_collection(collection_name):
 
 
 def merge_collections():
-    files = ["servicos_perguntas_respostas_1746_2.csv", "servicos_perguntas_respostas_carioca-digital_2.csv"]
+    files = ["servicos_perguntas_respostas_1746_GPT.csv", "servicos_perguntas_respostas_carioca-digital_GPT.csv"]
     dfs = []
 
     for file in files:
@@ -271,24 +386,33 @@ def merge_collections():
 
     if dfs:
         df_merged = pd.concat(dfs, ignore_index=True)
-        df_merged.to_csv("servico_perguntas_respostas.csv", index=False, encoding="utf-8")
-        print("Arquivo salvo como 'servicos_perguntas_respostas.csv'")
+        df_merged.to_csv("servico_perguntas_respostas_GPT.csv", index=False, encoding="utf-8")
+        print("Arquivo salvo como 'servicos_perguntas_respostas_GPT.csv'")
 
 
 def generate_questions_csv(collection_name):
     # export_collections_to_csv(collection)
 
     drop_columns = ["embedding", "link_acesso", "ultima_atualizacao", "url", "tipo", "id_carioca_digital"] if collection_name == "carioca-digital" else ["embedding", "ultima_atualizacao", "url", "tipo"]
-    df = pd.read_csv(f"exported_typesense_{collection_name}_data.csv", encoding="utf-8").drop(columns=drop_columns)
+    df = pd.read_csv(f"/home/brunoalmeida/projects/ed_rio/app-eai-agent/src/evaluations/letta/phoenix/training_dataset/exported_typesense_{collection_name}_data.csv", encoding="utf-8").drop(columns=drop_columns)
+
+    if collection_name == "carioca-digital":
+        df = df[df['titulo'].isin(TOP_SERVICOS_CARIOCA_DIGITAL)]
+    elif collection_name == "1746":
+        df = df[df['titulo'].isin(TOP_SERVICOS_1746)]
+    else:
+        print(f"Coleção {collection_name} não reconhecida. Pulando...")
+        return
+
     df["perguntas_geradas"] = df.progress_apply(generate_questions, axis=1)
-    df.to_csv(f"perguntas_baseadas_no_{collection_name}.csv", index=False)
+    df.to_csv(f"perguntas_baseadas_no_{collection_name}_GPT.csv", index=False)
 
 
 if __name__ == "__main__":
     collections = ["carioca-digital", "1746"]
 
     for collection in collections:
-        # generate_questions_csv(collection)
+        generate_questions_csv(collection)
         process_collection(collection)
 
     merge_collections()
--- src/evaluations/letta/phoenix/training_dataset/upload_dataset.py
+++ src/evaluations/letta/phoenix/training_dataset/upload_dataset.py
@@ -10,12 +10,12 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../.
 from src.config import env
 
 phoenix_client = px.Client(endpoint=env.PHOENIX_ENDPOINT)
-DATASET_PATH = "src/evaluations/letta/phoenix/training_dataset/servico_perguntas_respostas.csv"
+DATASET_PATH = "src/evaluations/letta/phoenix/training_dataset/dataset_treino_GPT.csv"
 training_dataset = pd.read_csv(DATASET_PATH, encoding="utf-8")
 
 keeping_columns = [
     "pergunta_individual",
-    "resposta_ideal_individual",
+    # "resposta_ideal_individual",
     "titulo",
     "descricao",
     "informacoes_complementares",
@@ -26,20 +26,20 @@ keeping_columns = [
 
 training_dataset = (
     training_dataset[keeping_columns]
-    .dropna(subset=["pergunta_individual", "resposta_ideal_individual"])
+    .dropna(subset=["pergunta_individual"])#, "resposta_ideal_individual"])
     .rename(
         columns={
             "pergunta_individual": "pergunta",
-            "resposta_ideal_individual": "resposta_ideal",
+            #"resposta_ideal_individual": "resposta_ideal",
         }
     )
 )
 
 now = datetime.now().strftime("%Y-%m-%d")
-dataset_name = f"Typesense_IA_Dataset-{now}"
+dataset_name = f"GPT_Dataset-{now}"
 
 dataset = phoenix_client.upload_dataset(
-    dataframe=training_dataset.sample(30), 
+    dataframe=training_dataset.head(50), 
     dataset_name=dataset_name, 
     input_keys=[
         "pergunta",
@@ -50,7 +50,7 @@ dataset = phoenix_client.upload_dataset(
         "resumo_whatsapp",
         "orgao_gestor",
     ], 
-    output_keys=["resposta_ideal"],
+    #output_keys=["resposta_ideal"],
 )
 
 print(f"Dataset '{dataset_name}' enviado com sucesso para o Phoenix!")
