582136d65b75a67ae0da4da07a1d2bc85bf9ed1a
d116626 <d116626@gmail.com>
2025-07-01 11:58:06 -0300

    fix: env conflicts

--- src/config/env.py
+++ src/config/env.py
@@ -1,8 +1,11 @@
 from src.utils.infisical import getenv_or_action
+import os
 
-import dotenv
+# if file .env exists, load it
+if os.path.exists("src/config/.env"):
+    import dotenv
 
-dotenv.load_dotenv(dotenv_path="src/config/.env")
+    dotenv.load_dotenv(dotenv_path="src/config/.env")
 
 
 LETTA_API_URL = getenv_or_action("LETTA_API_URL")
