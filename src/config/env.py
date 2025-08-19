bca1b2a7199d83487f83b4f209c6abb54d0f7cbb
Bruno Almeida <brunodepauloalmeida@gmail.com>
2025-08-19 12:36:42 -0300

    feat: add beta group whitelist configuration to env.py

--- src/config/env.py
+++ src/config/env.py
@@ -83,3 +83,16 @@ CIDADAO_ISSUER = getenv_or_action("CIDADAO_ISSUER", action="ignore")
 CIDADAO_CLIENT_ID = getenv_or_action("CIDADAO_CLIENT_ID", action="ignore")
 CIDADAO_CLIENT_SECRET = getenv_or_action("CIDADAO_CLIENT_SECRET", action="ignore")
 CIDADAO_API_BASE_URL = getenv_or_action("CIDADAO_API_BASE_URL", action="ignore")
+
+# Beta Group / Whitelist Configuration
+WHITELIST_API_BASE_URL_STAGING = getenv_or_action("WHITELIST_API_BASE_URL_STAGING", action="ignore")
+WHITELIST_ISSUER_STAGING = getenv_or_action("WHITELIST_ISSUER_STAGING", action="ignore")
+WHITELIST_CLIENT_ID_STAGING = getenv_or_action("WHITELIST_CLIENT_ID_STAGING", action="ignore")
+WHITELIST_CLIENT_SECRET_STAGING = getenv_or_action("WHITELIST_CLIENT_SECRET_STAGING", action="ignore")
+
+WHITELIST_API_BASE_URL_PROD = getenv_or_action("WHITELIST_API_BASE_URL_PROD", action="ignore")
+WHITELIST_ISSUER_PROD = getenv_or_action("WHITELIST_ISSUER_PROD", action="ignore")
+WHITELIST_CLIENT_ID_PROD = getenv_or_action("WHITELIST_CLIENT_ID_PROD", action="ignore")
+WHITELIST_CLIENT_SECRET_PROD = getenv_or_action("WHITELIST_CLIENT_SECRET_PROD", action="ignore")
+
+WHITELIST_GOOGLE_SHEET = getenv_or_action("WHITELIST_GOOGLE_SHEET", action="ignore")
